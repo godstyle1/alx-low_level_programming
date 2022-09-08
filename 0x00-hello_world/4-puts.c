44d64a1bbdb479694d2b4dd5b133ec56e6e700f6
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-08 21:11:36 +0000

    my low level programming commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - main block
  *
- * Return: Always 0 
+ * Return: 0 
  */
+
 int main(void)
+
 {
-	puts("Programming is like building a multilingual puzzle\n");
+
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	return (0);
+
 }
