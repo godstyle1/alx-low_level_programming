e5c6515b1d93a0647c8120c88e7e390b568f08dd
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-10 09:47:02 +0000

    my low level programming commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+
+	int s;
+
+	for (s = 'a'; s <= 'z'; s++)
+		putchar(s);
+
+	return (0);
+
+}
