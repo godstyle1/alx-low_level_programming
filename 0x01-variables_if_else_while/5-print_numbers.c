4e1aa240ea5286acfb8a79e6d6666016bd2f67ae
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-11 16:25:04 +0000

    my low level programming commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+	int j = '9';
+
+	for (a = '0'; a <= j; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
