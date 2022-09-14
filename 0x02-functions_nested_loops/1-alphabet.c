09643dfdded386454c6042869daed0976ae6d437
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-14 08:44:46 +0000

    my low level programming commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * alphabet_low - alphabet written in lowercase
+ *
+ * give new line after print
+ *
+ * Return: characters
+ */
+void alphabet_low(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar('a');
+		putchar('\n');
+	}
+}
