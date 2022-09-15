d7525ca03c927a164d3fc603f2564481fff1a96f
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-15 03:37:28 +0000

    my low level programming commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - The alphabet character entry point
+ *
+ * @c: character parameter for the argument
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	} else if (c >= 'A' && c <= 'Z')
+	{
+		return(1)
+	}else 
+	{ 
+		return (0) 
+	}
+}
