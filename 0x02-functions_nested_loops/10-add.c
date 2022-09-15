489654fe55a40df3eaa904dba73e11de43d131fe
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-15 15:12:15 +0000

    my low level programming commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adding of two integers
+ * @a: is the first of two integers to be added
+ * @b: is the second of two integers to be added
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	_putchar(sum + '0');
+	return (sum);
+}
