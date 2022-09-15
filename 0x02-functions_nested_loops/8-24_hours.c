36f3be78b78b6eeb0a6520dc4e58f9121ff254bf
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-15 14:14:09 +0000

    my low level programming commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Minutes of jack bauer life
+ *
+ * This function print every minute of jack bauer's day
+ *
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int hrs;
+	int min;
+	int hrs_remainder;
+	int min_remainder;
+	
+	while (hrs <= 23)
+	{
+	while (min <= 59)
+	{
+	min_remainder = min % 10;
+	hrs_remainder = hrs % 10;
+	_putchar(hrs / 10 + '0');
+	_putchar(hrs_remainder + '0');
+	_putchar(':');
+	_putchar(min / 10 + '0');
+	_putchar(min_remainder + 10);
+	min++;
+	_putchar('\n');
+	}
+	hrs++;
+	min = 0;
+	}
+}
