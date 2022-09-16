bab3bf80ac2b62aacd22f3c31f921070f6bc538c
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-16 20:05:53 +0000

    my more function nested loop commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - This function multiplies two integers together
+ * @a: this integer is the multiplicand
+ * @b: this integer is the multiplier
+ * Return: Alway 0.
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
