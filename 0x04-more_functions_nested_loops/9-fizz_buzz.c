6f52318ccdbde33fdd2920ca6c94994e3a282688
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-19 08:12:45 +0000

    my low level programming commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - to print fizzbuzz
+ * get fizzbuzz get fiz get buzz
+ * Return: nothing
+ */
+int main(void)
+{
+	int number = 0;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (((number % 3) == 0) && ((number % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((number % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((number % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+			printf("%d ", number);
+	}
+	printf("%c", '\n');
+	return (0);
+}
