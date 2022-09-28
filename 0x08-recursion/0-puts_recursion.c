6988e7dbbc915ba8fae22ea9da7ff9a90c46d418
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-28 18:48:29 +0000

    my recursion task commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string.
+ * *@s: pointer to a character parameter
+ *
+ * Return: void.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	_putchar('\n');
+}
