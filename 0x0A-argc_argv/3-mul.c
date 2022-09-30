ee8d3646ca569ecadd27d0c698ac961cccc75c09
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-30 15:42:02 +0000

    my argc and argv project commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - print its name follow by a new line
+ * @argc: argument count
+ * *@argv: argument strings
+ * Return: 0 to obtain path
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+	}
+	printf("%d\n", mul);
+	return (0);
+}
+
