a7bcb1ef27ab0881055ec0830075ace40211e083
godstyle1 <dreamdfuture2001@gmail.com>
2022-09-30 13:13:01 +0000

    my argc and argv project commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print its name follow by a new line
+ * @argc: argument count
+ * *@argv: argument strings
+ * Return: 0 to obtain path
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
