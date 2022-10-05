246f00c2fb4e1d0f2f8d3c6e9345dfc884308e97
godstyle1 <dreamdfuture2001@gmail.com>
2022-10-05 19:01:14 +0000

    my argc and argv project commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc: argument counter
+ * @argv: pointer to character array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
